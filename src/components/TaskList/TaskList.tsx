8a80366c192f1e66766db81ea34920e46a441ae8
Abenezer <abitysank@gmail.com>
2025-12-09 19:56:56 -0500

    TaskList basics implemented

---
+++ src/components/TaskList/TaskList.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import type { TaskListProps } from "../../types";
+import { TaskItem } from "../TaskItem/TaskItem";
+
+export const TaskList: React.FC<TaskListProps> = ({
+    tasks,
+    onStatusChange,
+    onDelete,
+}) => {
+    if (tasks.length === 0) {
+        return (
+            <div>
+                <div>No tasks Found</div>
+            </div>
+        );
+    }
+    return (
+        <div>
+            {tasks.map((task) => (
+                <TaskItem
+                    key={task.id}
+                    task={task}
+                    onStatusChange={onStatusChange}
+                    onDelete={onDelete}
+                />
+            ))}
+        </div>
+    );
+};
